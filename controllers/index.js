e941447554d6b4605632ad8d5cc533f9970bec7e
ygrenzinger <yannick.grenzinger@gmail.com>
2014-11-02 18:49:21 +0100

    Working on admin authentication

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,31 @@
 var passport = require('passport'),
     mail = require('../lib/mail.js');
 
+var addOauthProvider = function(provider, router) {
+
+    router.get('/auth/'+provider, passport.authenticate(provider, { scope: [ 'email' ] }));
+    router.get('/auth/'+provider+'/callback', function(req, res, next){
+        passport.authenticate(provider, function(err, user, info){
+            // This is the default destination upon successful login.
+            var redirectUrl = '/';
+
+            if (err) { return next(err); }
+            if (!user) { return res.redirect('/login'); }
+            req.logIn(user, function(err) {
+                if (err) { return next(err); }
+
+                // If we have previously stored a redirectUrl, use that,
+                // otherwise, use the default.
+                if (req.session.goingTo) {
+                    redirectUrl = req.session.goingTo;
+                    req.session.goingTo = null;
+                }
+                res.redirect(redirectUrl);
+            });
+        })(req, res, next);
+    });
+};
+
 module.exports = function (router) {
 
     router.get('/', function (req, res) {
@@ -13,15 +38,8 @@ module.exports = function (router) {
         res.render('profile');
     });
 
-    router.get('/auth/facebook', passport.authenticate('facebook', { scope: [ 'email' ] }));
-    router.get('/auth/facebook/callback',
-        passport.authenticate('facebook', { successRedirect: '/',
-            failureRedirect: '/login' }));
-
-    router.get('/auth/google', passport.authenticate('google', { scope: [ 'email' ] }));
-    router.get('/auth/google/callback',
-        passport.authenticate('google', { successRedirect: '/',
-            failureRedirect: '/login' }));
+    addOauthProvider('google', router);
+    addOauthProvider('facebook', router);
 
     router.get('/mailTest', function(req, res) {
         mail.send('yannick.grenzinger@gmail.com', 'test', 'hello world mail');
--- models/userModel.js
+++ models/userModel.js
@@ -3,54 +3,74 @@
  */
 'use strict';
 var mongoose = require('mongoose'),
-  bcrypt = require('bcrypt');
+    bcrypt = require('bcrypt'),
+    Q = require('q');
 
-var userModel = function() {
+var userModel = function () {
 
-  var userSchema = mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    email: {
-      type: String,
-      unique: true,
-      required: true
-    },
-    password: String, //We'll store bCrypt hashed passwords.  Just say no to plaintext!
-    role: String
-  });
-
-  /**
-   * Helper function that hooks into the 'save' method, and replaces plaintext passwords with a hashed version.
-   */
-  userSchema.pre('save', function(next) {
-    var user = this;
-
-    //If the password has not been modified in this save operation, leave it alone (So we don't double hash it)
-    if (!user.password || !user.isModified('password')) {
-      next();
-      return;
-    }
-
-    //Encrypt it using bCrypt. Using the Sync method instead of Async to keep the code simple.
-    //Replace the plaintext pw with the Hash+Salted pw;
-    user.password = bcrypt.hashSync(user.password, 8);
-
-    //Continue with the save operation
-    next();
-  });
-
-  /**
-   * Helper function that takes a plaintext password and compares it against the user's hashed password.
-   * @param plainText
-   * @returns true/false
-   */
-  userSchema.methods.passwordMatches = function(plainText) {
-    var user = this;
-    return bcrypt.compareSync(plainText, user.password);
-  };
+    var userSchema = mongoose.Schema({
+        email: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        displayname: String,
+        firstname: String,
+        lastname: String,
+        password: String, //We'll store bCrypt hashed passwords.  Just say no to plaintext!
+        role: String
+    });
 
 
-  return mongoose.model('User', userSchema);
+    /**
+     * Helper function that takes a plaintext password and compares it against the user's hashed password.
+     * @param plainText
+     * @returns true/false
+     */
+    userSchema.methods.passwordMatches = function (plainText) {
+        var user = this;
+        return bcrypt.compareSync(plainText, user.password);
+    };
+
+
+    return mongoose.model('User', userSchema);
 };
 
-module.exports = new userModel();
+var model =  new userModel();
+module.exports.model = model;
+
+module.exports.findByEmail = function (email) {
+    var deferred = Q.defer();
+    model.findOne({
+        email: email
+    }, function (err, user) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve(user);
+        }
+    });
+    return deferred.promise;
+};
+
+module.exports.upsert = function (user, isNewPassword) {
+    var deferred = Q.defer();
+    if (isNewPassword && user.password) {
+        user.password = bcrypt.hashSync(user.password, 8);
+    }
+
+    if (user.email === 'absainte@yahoo.fr' || user.email === 'yannick.grenzinger@gmail.com') {
+        user.role = 'admin';
+    } else {
+        user.role = 'client';
+    }
+
+    model.findOneAndUpdate({email: user.email}, user, {upsert: true}, function (err, userFromDB) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve(userFromDB);
+        }
+    });
+    return deferred.promise;
+};
