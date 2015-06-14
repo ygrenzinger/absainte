028278317ee31a8d61f1ca17950c2c4d56505269
ygrenzinger <yannick.grenzinger@gmail.com>
2015-06-14 18:56:56 +0200

    Adding filters for product

--- public/js/admin/controllers/products.js
+++ public/js/admin/controllers/products.js
@@ -1,11 +1,18 @@
 'use strict';
 
 controllers.controller('ProductsCtrl', ['$scope', '$http',
-  function($scope, $http) {
-    $scope.products = [];
-    $http({method: 'GET', url: '/admin/products'}).
-      success(function(data, status, headers, config) {
-        $scope.products = data;
-      });
-  }
+    function ($scope, $http) {
+        $scope.products = [];
+
+        $scope.selectedCollection = null;
+        $http({method: 'GET', url: '/admin/collections/'}).
+            success(function (data) {
+                $scope.collections = data;
+            });
+
+        $http({method: 'GET', url: '/admin/products'}).
+            success(function (data) {
+                $scope.products = data;
+            });
+    }
 ]);
