d0e5e2b13adad3bd832a47f5cb4a03077fb283d7
ygrenzinger <yannick.grenzinger@gmail.com>
2015-06-28 19:06:50 +0200

    JS correcton for JSHint

--- controllers/admin/products/index.js
+++ controllers/admin/products/index.js
@@ -3,31 +3,31 @@
 var ProductModel = require('../../../models/productModel.js'),
     _ = require('lodash');
 
-module.exports = function(router) {
+module.exports = function (router) {
 
-  router.get('/', function(req, res) {
-      ProductModel
-          .findAll()
-          .then(function (products) {
-              res.send(products);
-          })
-          .fail(function (err) {
-              res.send(500, err);
-          });
-  });
+    router.get('/', function (req, res) {
+        ProductModel
+            .findAll()
+            .then(function (products) {
+                res.send(products);
+            })
+            .fail(function (err) {
+                res.send(500, err);
+            });
+    });
 
-  router.get('/:permalink', function(req, res) {
-      ProductModel
-          .findByPermalink(req.params.permalink)
-          .then(function (product) {
-              res.send(product);
-          })
-          .fail(function (err) {
-              res.send(500, err);
-          });
-  });
+    router.get('/:permalink', function (req, res) {
+        ProductModel
+            .findByPermalink(req.params.permalink)
+            .then(function (product) {
+                res.send(product);
+            })
+            .fail(function (err) {
+                res.send(500, err);
+            });
+    });
 
-    router.delete('/:id', function(req, res) {
+    router.delete('/:id', function (req, res) {
         ProductModel
             .delete(req.params.id)
             .then(function () {
@@ -38,51 +38,54 @@ module.exports = function(router) {
             });
     });
 
-  var validProduct = function(req, res) {
-      if (!req.body.collectionFrom || !req.body.collectionFrom._id) {
-          res.status(400).send('Associated collection is missing');
-          return false;
-      }
-      if (!req.body.name) {
-          res.status(400).send('Product name is missing');
-          return false;
-      }
-      if (isNaN(req.body.price) || req.body.price < 0) {
-          res.status(400).send('Price is not correct');
-          return false;
-      }
-      if (!req.body.mainImage._id) {
-          res.status(400).send('Main image is missing');
-          return false;
-      }
-      return true;
-  };
+    var validProduct = function (req, res) {
+        if (!req.body.collectionFrom || !req.body.collectionFrom._id) {
+            res.status(400).send('Associated collection is missing');
+            return false;
+        }
+        if (!req.body.name) {
+            res.status(400).send('Product name is missing');
+            return false;
+        }
+        if (isNaN(req.body.price) || req.body.price < 0) {
+            res.status(400).send('Price is not correct');
+            return false;
+        }
+        if (!req.body.mainImage._id) {
+            res.status(400).send('Main image is missing');
+            return false;
+        }
+        return true;
+    };
 
-  router.post('/', function(req, res) {
-      if (validProduct(req, res)) {
+    router.post('/', function (req, res) {
+        if (!validProduct(req, res)) {
+            return;
+        }
 
-          var product = {
-              _id: req.body._id,
-              collectionFrom: req.body.collectionFrom._id,
-              name: req.body.name,
-              permalink: req.body.permalink,
-              price: req.body.price,
-              mainImage: req.body.mainImage._id,
-              description: {
-                  'en': req.body.description.en,
-                  'fr': req.body.description.fr
-              },
-              otherImages: _.map(req.body.otherImages, function(img) { return img._id; })
-          };
+        var product = {
+            _id: req.body._id,
+            collectionFrom: req.body.collectionFrom._id,
+            name: req.body.name,
+            permalink: req.body.permalink,
+            price: req.body.price,
+            mainImage: req.body.mainImage._id,
+            description: {
+                'en': req.body.description.en,
+                'fr': req.body.description.fr
+            },
+            otherImages: _.map(req.body.otherImages, function (img) {
+                return img._id;
+            })
+        };
 
-          ProductModel
-              .upsert(product)
-              .then(function (product) {
-                  res.send(product);
-              })
-              .fail(function (err) {
-                  res.status(500).send(err);
-              });
-      }
-  });
+        ProductModel
+            .upsert(product)
+            .then(function (product) {
+                res.send(product);
+            })
+            .fail(function (err) {
+                res.status(500).send(err);
+            });
+    });
 };
