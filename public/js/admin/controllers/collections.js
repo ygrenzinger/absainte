0c2da7fd56be12b95e679c78faa17c92bc9cd272
ygrenzinger <yannick.grenzinger@gmail.com>
2015-12-20 20:28:50 +0100

    Possibility of removing collection or image

--- public/js/admin/controllers/collections.js
+++ public/js/admin/controllers/collections.js
@@ -7,5 +7,48 @@ controllers.controller('CollectionsCtrl', ['$scope', '$http',
       success(function(data, status, headers, config) {
         $scope.collections = data;
       });
+
+
+
+      var loadAll = function () {
+          $http({method: 'GET', url: '/admin/collections'}).
+          success(function (data) {
+              $scope.collections = data;
+          });
+      };
+      loadAll();
+
+      $scope.delete = function (collection) {
+          var modalInstance = $modal.open({
+              templateUrl: 'collectionDeleteModal.html',
+              controller: ['$scope', '$modalInstance', 'collection',
+                  function ($scope, $modalInstance, collection) {
+                      $scope.collection = collection;
+
+                      $scope.ok = function () {
+                          $modalInstance.close($scope.collection);
+                      };
+
+                      $scope.cancel = function () {
+                          $modalInstance.dismiss('cancel');
+                      };
+                  }],
+              resolve: {
+                  collection: function () {
+                      return collection;
+                  }
+              }
+          });
+
+          modalInstance.result.then(function (collection) {
+              $http({method: 'DELETE', url: '/admin/collections/' + collection._id}).
+              success(function () {
+                  $log.info('Deleted ' + collection.name + ' deleted');
+                  loadAll();
+              });
+          }, function () {
+              $log.info('Delete Modal dismissed');
+          });
+      };
   }
 ]);
--- public/js/admin/controllers/images.js
+++ public/js/admin/controllers/images.js
@@ -1,11 +1,48 @@
 'use strict';
 
 controllers.controller('ImagesCtrl', ['$scope', '$http',
-  function($scope, $http) {
-    $scope.images = [];
-    $http({method: 'GET', url: '/admin/images'}).
-      success(function(data, status, headers, config) {
-        $scope.images = data;
-      });
-  }
+    function ($scope, $http) {
+        $scope.images = [];
+
+        var loadAll = function () {
+            $http({method: 'GET', url: '/admin/images'}).
+            success(function (data) {
+                $scope.images = data;
+            });
+        };
+        loadAll();
+
+        $scope.delete = function (image) {
+            var modalInstance = $modal.open({
+                templateUrl: 'imageDeleteModal.html',
+                controller: ['$scope', '$modalInstance', 'image',
+                    function ($scope, $modalInstance, image) {
+                        $scope.image = image;
+
+                        $scope.ok = function () {
+                            $modalInstance.close($scope.image);
+                        };
+
+                        $scope.cancel = function () {
+                            $modalInstance.dismiss('cancel');
+                        };
+                    }],
+                resolve: {
+                    image: function () {
+                        return image;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (image) {
+                $http({method: 'DELETE', url: '/admin/images/' + image._id}).
+                success(function () {
+                    $log.info('Deleted ' + image.name + ' deleted');
+                    loadAll();
+                });
+            }, function () {
+                $log.info('Delete Modal dismissed');
+            });
+        };
+    }
 ]);
